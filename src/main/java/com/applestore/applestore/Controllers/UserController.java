ccacbf32d7a35833c5aa43e5e0ae606c091c01aa
thanhtruong03 <phamthanhtruong400@gmail.com>
2024-01-26 00:00:26 +0700

    Dev Branch 2

--- src/main/java/com/applestore/applestore/Controllers/UserController.java
+++ src/main/java/com/applestore/applestore/Controllers/UserController.java
@@ -1,4 +1,13 @@
 package com.applestore.applestore.Controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
 public class UserController {
+    @GetMapping("/hi")
+    public String hello(){
+        return "hello";
+    }
 }
